b982f502ee08aba378fdfef4485561c5a2792d35
roubzy1704 <roubzy1704@yahoo.com>
2020-05-07 19:40:27 -0500

    Updates

---
+++ precache-manifest.fd02b5f554fe7497f60904fc197a06da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2358577e39ce1a9797ff77e71ac803b",
+    "url": "/wedding_design_concept/index.html"
+  },
+  {
+    "revision": "9b6b549dbde3b5b271cf",
+    "url": "/wedding_design_concept/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "779bdd623b2becb2a3a1",
+    "url": "/wedding_design_concept/static/js/2.c46f420e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wedding_design_concept/static/js/2.c46f420e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6b549dbde3b5b271cf",
+    "url": "/wedding_design_concept/static/js/main.5c05d42b.chunk.js"
+  },
+  {
+    "revision": "81dbef7205c65a92d8ac",
+    "url": "/wedding_design_concept/static/js/runtime-main.127a726d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wedding_design_concept/static/media/logo.5d5d9eef.svg"
+  }
+]);
