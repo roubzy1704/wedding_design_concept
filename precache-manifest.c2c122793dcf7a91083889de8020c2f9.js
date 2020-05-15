8dbc84a4a8125b2663ed80faca475f14abda7eba
roubzy1704 <roubzy1704@yahoo.com>
2020-05-15 18:56:00 -0500

    Updates

---
+++ precache-manifest.c2c122793dcf7a91083889de8020c2f9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1379ebe59f08dce001722d67cb4594ea",
+    "url": "/wedding_design_concept/index.html"
+  },
+  {
+    "revision": "9d1e7e627e421550fafb",
+    "url": "/wedding_design_concept/static/css/main.820c4965.chunk.css"
+  },
+  {
+    "revision": "05c1d4034ec35be444df",
+    "url": "/wedding_design_concept/static/js/2.ae3e0815.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/wedding_design_concept/static/js/2.ae3e0815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1e7e627e421550fafb",
+    "url": "/wedding_design_concept/static/js/main.8ec88223.chunk.js"
+  },
+  {
+    "revision": "81dbef7205c65a92d8ac",
+    "url": "/wedding_design_concept/static/js/runtime-main.127a726d.js"
+  },
+  {
+    "revision": "8657ce9482be5d00f4eb0ec9ddc720fd",
+    "url": "/wedding_design_concept/static/media/Image1.8657ce94.jpg"
+  },
+  {
+    "revision": "d546741fb132b0c4573612cce08ed047",
+    "url": "/wedding_design_concept/static/media/Image10.d546741f.jpg"
+  },
+  {
+    "revision": "a6bc8141762acacd515d2c8bf20ea624",
+    "url": "/wedding_design_concept/static/media/Image2.a6bc8141.jpg"
+  },
+  {
+    "revision": "2148f8bb950c8943da8e4a659662a4d6",
+    "url": "/wedding_design_concept/static/media/Image3.2148f8bb.jpg"
+  },
+  {
+    "revision": "5276e5fd5f4761e7409f4c602e347ff2",
+    "url": "/wedding_design_concept/static/media/Image4.5276e5fd.jpg"
+  },
+  {
+    "revision": "3378e721ce136c8900281debafc292fd",
+    "url": "/wedding_design_concept/static/media/Image5.3378e721.jpg"
+  },
+  {
+    "revision": "c91974506228f34862a955c64d382b6b",
+    "url": "/wedding_design_concept/static/media/Image6.c9197450.jpg"
+  },
+  {
+    "revision": "43224d73de2789fd5bd2b31978c9206f",
+    "url": "/wedding_design_concept/static/media/Image7.43224d73.jpg"
+  },
+  {
+    "revision": "7a2d00983652851455ca1372c9e102d3",
+    "url": "/wedding_design_concept/static/media/Image8.7a2d0098.jpg"
+  },
+  {
+    "revision": "da58f447934b2f79bc95864235820b93",
+    "url": "/wedding_design_concept/static/media/Image9.da58f447.jpg"
+  },
+  {
+    "revision": "fa51399d7074162801b350dd14de551e",
+    "url": "/wedding_design_concept/static/media/amazon-logo.fa51399d.png"
+  },
+  {
+    "revision": "f2345ca97bf0c62df1e2f4bf0fea37e7",
+    "url": "/wedding_design_concept/static/media/bedBathBeyond.f2345ca9.png"
+  },
+  {
+    "revision": "2f233edbd89b25813632b4a2c055a92e",
+    "url": "/wedding_design_concept/static/media/crateandbarrel.2f233edb.png"
+  },
+  {
+    "revision": "ad3f0d1406eb8c61db12ff36be9ce82c",
+    "url": "/wedding_design_concept/static/media/food52.ad3f0d14.png"
+  },
+  {
+    "revision": "d7f47d81b934b4bed635dce682e51b5a",
+    "url": "/wedding_design_concept/static/media/macys.d7f47d81.png"
+  },
+  {
+    "revision": "53eb3c48dcf2c58b72be2b5b86b5cd27",
+    "url": "/wedding_design_concept/static/media/target.53eb3c48.png"
+  },
+  {
+    "revision": "d321857ed91efa5152dcb2d9f9fda51c",
+    "url": "/wedding_design_concept/static/media/zola.d321857e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wedding_design_concept/precache-manifest.201c99f77bce58106a08853630395969.js"
+  "/wedding_design_concept/precache-manifest.c2c122793dcf7a91083889de8020c2f9.js"
 );
 
 self.addEventListener('message', (event) => {
